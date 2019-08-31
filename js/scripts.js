5b1e60018dd0f65fb5229dde3f7f7cfeda2dd28f
Lauren <Lauren.Musante@gmail.com>
2019-08-31 14:12:50 -0700

    Add various methods and functions to calculate toppings price seperately from size price. Start to test all options.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,76 @@
 //Back end
-var pizza; //declaring object called pizza
 
-var pizza;
-pizza.toppings = [];
-
-// //UI
 $(document).ready(function(){
   $("form#pizzaForm").submit(function(event){
     event.preventDefault();
-    $("#showPizza").show();
-    // $(".showPrice").show();
-    $("input:checkbox[name=toppings]:checked").each(function(){
-      var toppings = $(this).val();
-      pizza.toppings.push(toppings);
-      $('#showPizza').append(toppings + "<br>");
-      $('#pizzaForm').hide();
 
-    });
-  });
+//to calculate size price
+      var size = $("input:radio[name=size]:checked").val();
+      // var medium = $("input:radio[name=size]:checked").val();
+      // var large = $("input:radio[name=size]:checked").val();
+      // var xlarge = $("input:radio[name=size]:checked").val();
+//to calculate size price
+if (size = "small"){
+  return "$5" // return? or this.price= ?
+  console.log(size)
+} else if (size = "medium"){
+  this.price="$7"
+} else if (size = "large"){
+  this.price="$10"
+} else
+  this.price="$13"
 });
+
+//declare Object
+function Pizza (size, toppings) {
+  size = size,
+  toppingsArray = toppings,
+  sizePrice = size price,
+  toppingsPrice = toppings price
+}
+
+//make toppingsArray
+var toppingsArray = [];
+$("input:checkbox[name=toppings]:checked").each(function(){
+  var toppings = $(this).val();
+  this.toppingsArray.push(toppings); //this.? Do I need?
+}):
+
+
+
+var workTransportationMode = $(this).val();
+   $('#work-responses').append(workTransportationMode + "<br>");
+ });
+ $('#transportation_survey').hide();
+});
+//function to calculate toppings price
+Pizza.prototype.calcToppings = function(){
+  var price=0;
+  var toppingsArray=[]; //pizza.toppings?
+  this.toppingsArray=[]; //<----?
+  for (var i =0; i => this.toppingsArray.length; i++){
+  this.price += .5; //can also just do if length === 1, $x; if length === 2, $y,
+}
+return this.toppingsPrice //with or without "this."?
+}
+
+Pizza.prototype.calcTotal= function(){
+  return this.toppingsPrice + this.sizePrice // with or without "this."?"
+}
+
+});
+// // //UI
+// $(document).ready(function(){
+//   $("form#pizzaForm").submit(function(event){
+//     event.preventDefault();
+//     $("#showPizza").show();
+//     // $(".showPrice").show();
+//     $("input:checkbox[name=toppings]:checked").each(function(){
+//       var toppings = $(this).val();
+//       pizza.toppings.push(toppings);
+//       $('#showPizza').append(toppings + "<br>");
+//       $('#pizzaForm').hide();
+//
+//     });
+//   });
+// });
