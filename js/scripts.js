b14bf84bd04f348af70747dc324f50efb7178229
Lauren <Lauren.Musante@gmail.com>
2019-09-04 20:59:25 -0700

    Edit radio button names in HTML, add a div to show/hide price result, attempt to separate UI and backend logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,51 @@
-//Back end
+
+
+function Pizza (size, toppings) {
+  this.size = size,
+  this.toppings = toppings,
+  this.sizePrice = 0,
+  this.totalCost = " "//??
+}
+console.log(Pizza);
+
+var newPizza = new Pizza (this.size, this.toppings, this.sizePrice, this.totalCost)
+console.log(newPizza);
+
+var size = $("input:radio[name='size']:checked").val();
+console.log(size); // returns "undefined"
+Pizza.prototype.sizePrice = function() {
+  if (size = "small"){
+    this.sizePrice += 5
+  } else if  (size = "medium") {
+    this.sizePrice += 7
+  } else if (size = "large") {
+    this.sizePrice += 10
+    console.log (this.sizePrice);
+  } else {
+      this.sizePrice += 13
+    }
+    console.log(this.sizePrice);
+  };
+
+Pizza.prototype.calcToppings = function(){
+    var toppingsPrice = 0;
+    toppingsArray.forEach(function(toppings){
+    toppingsPrice += 0.5
+  });
+  newPizza.calcToppings();
+  console.log(newPizza.calcToppings)
+};
+
 $(document).ready(function(){
   $("form#pizzaForm").submit(function(event){
     event.preventDefault();
-
-
-    //declare Object
-
-    function Pizza (size, toppings) {
-      this.size = size,
-      this.toppings = toppings,
-      // sizePrice = size price,
-      // toppingsPrice = toppings price
-    }
-
-//to calculate size price
-      var size = " "; // ????
-      var sizePrice = 0; //????
-      var small = $("input:radio[name=small]:checked").val();
-      var medium = $("input:radio[name=medium]:checked").val();
-      var large = $("input:radio[name=large]:checked").val();
-      var xlarge = $("input:radio[name=xlarge]:checked").val();
-
-//to calculate size price
-Pizza.prototype.sizePrice = function() {
-  if (size = "small"){
-    return "$5";
-  } else if  (size = "medium") {
-    return "$7";
-  } else if (size = "large") {
-    return "$10"
-  } else {
-      return "13"
-    }
-  };
-
-
-
-//function to calculate toppings price
-Pizza.prototype.calcToppings = function(){
-  var price=0;
-  var toppingsArray = [];
-  $("input:checkbox[name=toppings]:checked").each(function(){
-    var toppings = $(this).val();
-    this.toppingsArray.push(toppings);
+    var toppingsArray = [];
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      var toppings = $(this).val();
+      console.log(toppings);
+      this.toppingsArray.push(toppings); //console showing "push" as undefined. Also tried "append" with same result.
+    });
+  $(".result").show();
   });
-  this.toppingsArray //this will be the array that was passed into Pizza
-  for (var i =0; i => this.toppingsArray.length; i++){
-  price += .5;
-}
-return price
-}
-
-Pizza.prototype.calcTotal= function(){
-  return this.calcToppngs + this.sizePrice // still need to compute size price.
-}
-
 });
+    // new Pizza
